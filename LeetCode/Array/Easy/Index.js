243386b53af9259e957ef1048bbba16f14ec424c
Nitish Kumar <nitkumar30@WKMZT49F89C6.global.publicisgroupe.net>
2023-07-21 12:41:05 +0530

    Leeetcode medium

--- LeetCode/Array/Easy/Index.js
+++ LeetCode/Array/Easy/Index.js
@@ -192,7 +192,261 @@
 // console.log(sumOfMultiples(10));
 
 //===============================================================================
+
+//1528. Shuffle String
+
+// var restoreString = function (s, indices) {
+//   let newArr = new Array(indices.length);
+
+//   for (let i = 0; i < indices.length; i++) {
+//     newArr[indices[i]] = s[i];
+//   }
+//   return newArr.join("");
+// };
+
+// console.log(restoreString("codeleet", [4, 5, 6, 7, 0, 2, 1, 3]));
+
 //===============================================================================
+
+//1773. Count Items Matching a Rule
+
+// var countMatches = function (items, ruleKey, ruleValue) {
+//   let count = 0;
+
+//   let ruleKeyObj = {
+//     type: 0,
+//     color: 1,
+//     name: 2,
+//   };
+
+//   let index = ruleKeyObj[ruleKey];
+//   for (let i = 0; i < items.length; i++) {
+//     if (items[i][index] === ruleValue) {
+//       count++;
+//     }
+//   }
+//   return count;
+// };
+
+// console.log(
+//   countMatches(
+//     [
+//       ["phone", "blue", "pixel"],
+//       ["computer", "silver", "phone"],
+//       ["phone", "gold", "iphone"],
+//     ],
+//     "type",
+//     "phone"
+//   )
+// );
+
+//===============================================================================
+//2535. Difference Between Element Sum and Digit Sum of an Array
+
+// var differenceOfSum = function (nums) {
+//   let sum = nums.reduce((sum, e) => (sum += e), 0);
+//   let digitSum = nums
+//     .join("")
+//     .split("")
+//     .reduce((sum, e) => (sum += parseInt(e)), 0);
+//   return Math.abs(sum - digitSum);
+// };
+
+// console.log(differenceOfSum([1, 15, 6, 3]));
+
+//===============================================================================
+//1588. Sum of All Odd Length Subarrays
+
+// const getSum = (arr) => arr.reduce((sum, e) => (sum += e), 0);
+
+// const divideArray = (arr, n) => {
+//   let result = [];
+//   while (arr.length) {
+//     result.push(arr.splice(0, n));
+//   }
+
+//   return result;
+// };
+
+// var sumOddLengthSubarrays = function (arr) {};
+// console.log(sumOddLengthSubarrays([1, 4, 2, 5, 3]));
+
+//===============================================================================
+
+//1572. Matrix Diagonal Sum
+
+// var diagonalSum = function (mat) {
+//   let sum = 0;
+//   let length = mat.length;
+//   for (let i = 0; i < length; i++) {
+//     let j = length - 1 - i;
+
+//     if (i === j) {
+//       sum += mat[i][i];
+//     } else {
+//       sum += mat[i][i];
+//       sum += mat[i][j];
+//     }
+//   }
+
+//   return sum;
+// };
+
+// console.log(
+//   diagonalSum([
+//     [1, 1, 1, 1],
+//     [1, 1, 1, 1],
+//     [1, 1, 1, 1],
+//     [1, 1, 1, 1],
+//   ])
+// );
+
+//===============================================================================
+// 2006. Count Number of Pairs With Absolute Difference K
+
+// var countKDifference = function (nums, k) {
+//   let cont = 0;
+//   for (let i = 0; i < nums.length - 1; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (Math.abs(nums[i] - nums[j]) == k) {
+//         cont++;
+//       }
+//     }
+//   }
+//   return cont;
+// };
+
+// console.log(countKDifference([3, 2, 1, 5, 4], 2));
+
+//===============================================================================
+
+//2656. Maximum Sum With Exactly K Elements
+
+// var maximizeSum = function (nums, k) {
+//   let max = Math.max(...nums);
+//   let sum = 0;
+
+//   for (let i = 0; i < k; i++) {
+//     sum += max;
+//     max = max + 1;
+//   }
+//   return sum;
+// };
+
+// console.log(maximizeSum([1, 2, 3, 4, 5], 3));
+
+//===============================================================================
+
+//804. Unique Morse Code Words
+
+// let alphabet = [
+//   ".-",
+//   "-...",
+//   "-.-.",
+//   "-..",
+//   ".",
+//   "..-.",
+//   "--.",
+//   "....",
+//   "..",
+//   ".---",
+//   "-.-",
+//   ".-..",
+//   "--",
+//   "-.",
+//   "---",
+//   ".--.",
+//   "--.-",
+//   ".-.",
+//   "...",
+//   "-",
+//   "..-",
+//   "...-",
+//   ".--",
+//   "-..-",
+//   "-.--",
+//   "--..",
+// ];
+
+// function getUniqueWord(word) {
+//   let str = "";
+
+//   for (let i = 0; i < word.length; i++) {
+//     let index = word.charCodeAt(i) - 97;
+//     str += alphabet[index];
+//   }
+//   return str;
+// }
+
+// var uniqueMorseRepresentations = function (words) {
+//   let count = 0;
+//   words.reduce((obj, word) => {
+//     let key = getUniqueWord(word);
+//     if (!obj[key]) {
+//       obj[key] = 1;
+//       count++;
+//     }
+
+//     return obj;
+//   }, {});
+
+//   return count;
+// };
+
+// console.log(uniqueMorseRepresentations(["gin", "zen", "gig", "msg"]));
+
+//===============================================================================
+
+//1684. Count the Number of Consistent Strings
+
+// var countConsistentStrings = function (allowed, words) {
+//   return words.filter((word) => {
+//     return [...word].every((e) => allowed.includes(e));
+//   }).length;
+// };
+
+// console.log(countConsistentStrings("ab", ["ad", "bd", "aaab", "baa", "badab"]));
+
+//===============================================================================
+
+// 2744. Find Maximum Number of String Pairs
+
+// var maximumNumberOfStringPairs = function (words) {
+//   let count = 0;
+//   for (let i = 0; i < words.length - 1; i++) {
+//     for (let j = i + 1; j < words.length; j++) {
+//       let reversed = words[j].split("").reverse().join("");
+
+//       if (words[i] === reversed) {
+//         count++;
+//       }
+//     }
+//   }
+//   return count;
+// };
+
+// console.log(maximumNumberOfStringPairs(["cd", "ac", "dc", "ca", "zz"]));
+
+//===============================================================================
+
+//832. Flipping an Image
+
+// var flipAndInvertImage = function (image) {
+//   return image
+//     .map((row) => row.reverse())
+//     .map((row) => {
+//       return row.map((e) => (e == 0 ? 1 : 0));
+//     });
+// };
+
+// let img = [
+//   [1, 1, 0],
+//   [1, 0, 1],
+//   [0, 0, 0],
+// ];
+
+// console.log(flipAndInvertImage(img));
+
 //===============================================================================
 //===============================================================================
 //===============================================================================
--- LeetCode/Array/Medium/Index.js
+++ LeetCode/Array/Medium/Index.js
@@ -1,4 +1,38 @@
 //===============================================================================
+// 3. Longest Substring Without Repeating Characters
+
+// var lengthOfLongestSubstring = function (s) {
+//   let max = 0;
+//   let word = "";
+
+//   // running a loop till length of string
+//   for (let i = 0; i < s.length; i++) {
+//     //Running a loop for each char in string
+//     for (let j = i; j < s.length; j++) {
+//       //If char present in word then jump our from loop
+//       if (word.includes(s[j])) {
+//         break;
+//       }
+
+//       //Storing word
+//       word += s[j];
+//     }
+
+//     //Replacing word with max
+//     if (max < word.length) {
+//       max = word.length;
+//     }
+
+//     //reset word for next iteration
+//     word = "";
+//   }
+
+//   //return max
+//   return max;
+// };
+
+// console.log(lengthOfLongestSubstring("dvdf"));
+
 //===============================================================================
 //===============================================================================
 //===============================================================================
